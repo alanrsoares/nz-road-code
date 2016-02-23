d0b6ecec4cde1bcec41c0460d0c2c1d34d1f93f3
Alan R. Soares <alanrsoars@gmail.com>
2016-02-24 00:01:25 +1300

    WIP: styling

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,17 @@
 import React, {
   View,
   Text,
-  Image
+  Image,
+  StyleSheet,
+  TouchableOpacity
 } from 'react-native'
 
 const renderAnswer = (key, answer) => (
-  <View key={key} style={{ padding: 5 }}>
+  <TouchableOpacity key={key} style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', padding: 5 }}>
     <Text>
       <Text style={{ fontWeight: 'bold', color: '#363' }}>{key}</Text>: {answer}
     </Text>
-  </View>
+  </TouchableOpacity>
 )
 
 const renderAnswers = (answers = {}) =>
@@ -17,19 +19,50 @@ const renderAnswers = (answers = {}) =>
         .map((key) => renderAnswer(key, answers[key]))
 
 export default ({ image, question, answers }) => (
-  <View>
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'flex-start', marginTop: 20 }}>
+  <View style={styles.topContainer}>
+    <View style={styles.topContainer}>
       <Image
         style={{ height: 200, width: 200 }}
-        source={image}/>
-      <View style={{ padding: 10 }}>
-        <Text style={{ fontWeight: 'bold' }}>
+        source={image}
+      />
+    <View style={styles.questionContainer}>
+        <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
           {question}
         </Text>
       </View>
     </View>
-    <View style={{ alignItems: 'flex-start', justifyContent: 'flex-start', marginTop: 10 }}>
+    <View style={styles.answersContainer}>
       {renderAnswers(answers)}
+    </View>
+    <View style={styles.topContainer}>
+      <TouchableOpacity style={styles.topContainer}>
+        <Text>Submit Answer</Text>
+      </TouchableOpacity>
     </View>
   </View>
 )
+
+const styles = StyleSheet.create({
+  topContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    marginHorizontal: 0,
+    borderWidth: 1
+  },
+  questionContainer: {
+    flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start',
+    margin: 10,
+    padding: 10,
+    borderWidth: 1
+  },
+  answersContainer: {
+    alignItems: 'stretch',
+    marginTop: 10
+  },
+  submitButtonContainer: {
+    flex: 1
+  }
+})
